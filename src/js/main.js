47b4eb39f76dd14f133d05e272129983225dea51
Roman <roman985475612@gmail.com>
2021-08-12 14:57:39 +0300

    Chapter 22 completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Works
+
 const worksList = document.querySelector('.works__list')
 
 worksList.addEventListener('focusin', e => {
@@ -11,4 +13,36 @@ worksList.addEventListener('focusout', e => {
         e.target.parentElement.parentElement.parentElement.classList.remove('works__item--active')
     }
 })
+
+
+// Skills
+
+function moveProgressBar(node, nodeLine, nodeTooltip, speed = 1500) {
+    const progressElements = document.querySelectorAll(node)
+
+    progressElements.forEach((elem) => {
+        let progress = elem.dataset.progress
+        let bar = elem.querySelector(nodeLine)
+        let tooltip = elem.querySelector(nodeTooltip)
+
+        bar.style.transitionDuration = (speed / 1000) + 's'
+        bar.style.width = progress + '%'
+
+        tooltip.innerHTML = progress + '%'
+        setTimeout(() => {
+            tooltip.classList.add('d-block')
+        }, speed)
+    })
+}
+
+const skills = document.querySelector('.skills')
+let animate = true
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset + 500 > skills.offsetTop && animate) {
+        moveProgressBar('.progress__element', '.progress__bar', '.progress__tooltip', 1500)
+        animate = false
+    } 
+})
+
 
